adf07cfcecaa23801b1f3da23822c53d6c6e3e87
Adam Johnson <AdamJohnso@gmail.com>
2013-01-11 22:08:30 -0500

    Convert IRC Bot to asyncore
    The IRCBotBase is borrowed from an old project of mine. It makes the code
    a lot simpler :)

--- aitrus.py
+++ aitrus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import asyncore
 import os, sys
 import threading
 import http.client
@@ -9,106 +10,23 @@ import time
 import getpass
 import base64
 
+from irc import *
+
 BOT_VERSION = "Aitrus 0.1"
 
-class ircbot:
-    def __init__(self, addr, port, nick, channel):
-        self.server = (addr, port)
-        self.nick = nick
-        self.channel = channel
-        self.inqueue = []
+class AitrusBot(IRCBotBase):
+    def handle_connect(self):
+        self.join_channel(sys.argv[4])
 
-        self.sock = None
-        self.connect()
+    def handle_ctcp(self, sender, msg):
+        if msg == "VERSION":
+            self.send_nctcp(sender, "%s {Python v%s}" % (BOT_VERSION, sys.version))
+        elif msg == "TIME":
+            self.send_nctcp(sender, "TIME keeps on slippin', slippin'")
 
-    def connect(self):
-        if self.sock:
-            self.sock.shutdown(socket.SHUT_RDWR)
-            self.sock.close()
-        self.sock = socket.create_connection(self.server, 60.0)
-        self.send('USER %s . . %s\r\n' % (self.nick, self.nick))
-        self.send('NICK %s\r\n' % self.nick)
-        self.send('JOIN #%s\r\n' % self.channel)
-
-    def send(self, data):
-        self.sock.send(bytes(data, 'utf8'))
-
-    def run(self):
-        packet = ''
-        bad_time = 0
-        good_time = 0
-        while True:
-            try:
-                packet += str(self.sock.recv(4096), 'utf8')
-            except socket.timeout:
-                good_time += 1
-                if good_time > 9:
-                    # Reconnect and try again
-                    bad_time += 1
-                    if bad_time > 3:
-                        print("Having trouble staying connected...  Bailing out!")
-                        sys.exit(1)
-                    self.connect()
-                    continue
-
-            for hub in self.inqueue:
-                self.sendMsg('#' + self.channel, hub)
-            self.inqueue = []
-
-            if len(packet) == 0:
-                continue
-
-            good_time = 0
-            bad_time = 0
-            lines = packet.split('\r\n')
-            for ln in lines[:-1]:
-                self.parse(ln)
-
-            packet = lines[-1] if len(lines[-1]) else ''
-
-    def parse(self, line):
-        if len(line) == 0:
-            return
-
-        sender = ''
-        msg = line.split(None, 3)
-        if line[0] == ':':
-            sender = msg[0][1:]
-            msg = msg[1:]
-        if len(msg) < 1:
-            return
-
-        if msg[0] == 'PING':
-            self.send('PONG ' + msg[1] + '\r\n')
-
-        elif msg[0] == 'PRIVMSG':
-            try:
-                recp = msg[1]
-                text = msg[2]
-            except IndexError:
-                print("Got bad message!")
-                return
-
-            snick = sender.split('!')[0]
-            if recp == self.nick:
-                dest = snick
-            else:
-                dest = recp
-
-            if text.startswith(':'):
-                text = text[1:]
-
-            # TODO: Commands?
-
-            if text.lower() == '\x01version\x01':
-                self.sendNotice(snick, '\x01VERSION ' + BOT_VERSION + '\x01')
-                return
-
-    def sendMsg(self, dest, msg):
-        self.send('PRIVMSG ' + dest + ' :' + msg + '\r\n')
-
-    def sendNotice(self, dest, msg):
-        self.send('NOTICE ' + dest + ' :' + msg + '\r\n')
+    def handle_kick(self, channel, reason):
+        # auto rejoin kicked channels
+        self.join_channel(channel)
 
 
 class hubber:
@@ -178,7 +96,7 @@ class hubber:
                     'user': pull['user']['login'],
                     'title': pull['title'],
                     }
-                self.irc.inqueue.append("%s has created pull request #%d (%s): %s" \
+                self.irc.send_wallchan("%s has created pull request #%d (%s): %s" \
                     % (self.pulls[pull_id]['user'], pull_id,
                        self.pulls[pull_id]['title'],
                        self.pulls[pull_id]['url']))
@@ -197,11 +115,11 @@ class hubber:
             jdata = json.loads(str(reply.read(), 'utf8'))
 
             if jdata['merged']:
-                self.irc.inqueue.append("%s has merged pull request #%d (%s)" \
+                self.irc.send_wallchan("%s has merged pull request #%d (%s)" \
                     % (jdata['merged_by']['login'], pull,
                        jdata['title']))
             else:
-                self.irc.inqueue.append("Pull request #%d (%s) has been closed" \
+                self.irc.send_wallchan("Pull request #%d (%s) has been closed" \
                     % (jdata['number'], jdata['title']))
 
             del self.pulls[pull]
@@ -225,7 +143,7 @@ class hubber:
                     'user': issue['user']['login'],
                     'title': issue['title'],
                     }
-                self.irc.inqueue.append("%s has created issue #%d (%s): %s" \
+                self.irc.send_wallchan("%s has created issue #%d (%s): %s" \
                     % (self.issues[issue_id]['user'], issue_id,
                        self.issues[issue_id]['title'],
                        self.issues[issue_id]['url']))
@@ -233,7 +151,7 @@ class hubber:
                 watch.remove(issue_id)
 
         for issue in watch:
-            self.irc.inqueue.append("Issue #%d (%s) has been closed" \
+            self.irc.send_wallchan("Issue #%d (%s) has been closed" \
                 % (issue, self.issues[issue]['title']))
 
             del self.issues[issue]
@@ -254,13 +172,12 @@ if len(sys.argv) < 6:
 host = sys.argv[1]
 port = int(sys.argv[2])
 nick = sys.argv[3]
-channel = sys.argv[4]
 hub_repos = sys.argv[5:]
 
 hub_user = input('Github Email/Username: ')
 hub_pass = getpass.getpass()
 
-irc = ircbot(host, port, nick, channel)
+irc = AitrusBot(host, port, nick)
 repos = []
 for repo in hub_repos:
     _hub = hubber(repo, irc, hub_user, hub_pass)
@@ -270,4 +187,4 @@ for repo in hub_repos:
 hub_th = threading.Thread(target=hub_watcher, args=(repos,))
 hub_th.daemon = True
 hub_th.start()
-irc.run()
+asyncore.loop()
---
+++ irc.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+import asynchat
+import socket
+
+class IRCBotBase:
+    class _net_core(asynchat.async_chat):
+        """Private net protocol implementation details"""
+        def __init__(self, parent, host, port):
+            self._parent = parent
+            self.timeout = 180
+            self.set_terminator(b"\r\n")
+            self.connected = False
+            self._incoming = []
+    
+            # asynchat socket init
+            asynchat.async_chat.__init__(self)
+            self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connect((host, port))
+
+        def collect_incoming_data(self, data):
+            self._incoming.append(data)
+
+        def send_cmd(self, msg):
+            if self.connected:
+                self.push(bytes(msg.join("\r\n"), "UTF-8"))
+            else:
+                raise NotConnectedError()
+
+        def handle_close(self):
+            raise NotImplementedError()
+
+        def handle_connect(self):
+            self.connected = True
+            self.send_cmd("NICK %s" % self._parent._nick_name)
+            self.send_cmd("USER aitrus 0 *:%s" % self._parent._real_name)
+            self._parent.handle_connect()
+
+        def handle_error(self):
+            raise # we want detailed errors
+
+        def found_terminator(self):
+            def get_message(raw, data, start):
+                trash = 1 # account for leading colon
+                for i in range(start):
+                    trash += len(data[i]) + 1 # account for space
+                return raw[trash:]
+
+            raw = (b"".join(self._incoming)).decode("UTF-8")
+            self._incoming = []
+            if raw.upper().startswith("PING"):
+                self.send_cmd("PONG %s" % raw[5:])
+                return
+            data = raw.split()
+            if len(data) < 3:
+                # This message is screwed up (too short)
+                return
+            sender = data[0].split("!")[0][1:]
+            cmd = data[1].upper()
+
+            # Let the ugly ass parsing begin!
+            if cmd.isdigit():
+                pass # Later...
+            elif cmd == "JOIN":
+                channel = data[2][1:]
+                self._parent._channels.append(channel)
+                self._parent.handle_join(channel)
+            elif cmd == "KICK":
+                channel = data[2] # no leading colon
+                reason = data[3][1:] # leading colon
+                self._parent._channels = [i for i in self._parent._channels if i != channel]
+                # FIXME: include kicker name?
+                self._parent.handle_kick(channel, reason)
+            elif cmd == "PRIVMSG":
+                msg = get_message(raw, data, 3)
+                if msg.startswith("\u0001"):
+                    if msg.startswith("\u0001ACTION"):
+                        expand = "%s %s" % (sender, msg[8:len(msg)-1])
+                        self._parent.handle_message(data[2], sender, expand)
+                    else:
+                        self._parent.handle_ctcp(sender, msg[1:len(msg)-1].upper())
+                else:
+                    self._parent.handle_message(data[2], sender, msg)
+
+
+    _channels = []
+
+    def __init__(self, host, port, nick="Aitrus", name="Aitrus Chat Bot"):
+        self._imp = IRCBotBase._net_core(self, host, port)
+        self._nick_name = nick
+        self._real_name = name
+
+    def handle_connect(self):
+        pass
+
+    def handle_ctcp(self, sender, msg):
+        pass
+
+    def handle_join(self, channel):
+        pass
+
+    def handle_kick(self, channel, reason):
+        pass
+
+    def handle_message(self, channel, user, msg):
+        pass
+
+    def join_channel(self, channel):
+        self._imp.send_cmd("JOIN %s" % channel)
+
+    def _set_nick_name(self, nick):
+        self._nick_name = nick
+        try:
+            self._imp.send_cmd("NICK %s" % nick)
+        except NotConnectedError:
+            pass # we'll set it when we connect
+    def _get_nick_name(self):
+        try:
+            return self._nick_name
+        except AttributeError:
+            return None
+    nick_name = property(_get_nick_name, _set_nick_name)
+
+    def send_nctcp(self, destination, msg):
+        self._imp.send_cmd("NOTICE %s :\u0001%s\u0001" % (destination, msg))
+
+    def send_msg(self, destination, msg):
+        self._imp.send_cmd("PRIVMSG %s :%s" % (destination, msg))
+
+    def send_quit(self, msg):
+        self._imp.send_cmd("QUIT :%s" % msg)
+
+    def send_wallchan(self, msg):
+        for chan in self._channels:
+            self.send_msg(chan, msg)
+
+
+class NotConnectedError(Exception):
+    """This error is raised when you attempt to do an operation that requires
+       a connection but no connection is established."""
+    pass
+
+
+# Test case
+if __name__ == '__main__':
+    import asyncore
+    ass = IRCBotBase("irc.justirc.net", 6667)
+    asyncore.loop()
